46d00cc43290d1db5713b0bca62c89377b166f5b
Sagar Padhye <sgr.m.pdy@gmail.com>
2015-01-03 23:51:55 +0530

    included readline as well

--- prompt.c
+++ prompt.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX 2048
-#define VERSIONINFO "clisp version 0.0.0.1"
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+#define VERSIONINFO "clisp version 0.0.0.2"
 #define PROMPT "clisp>"
-/* Buffer for user input */
-static char input[MAX];
 
 int main(int argc, char ** argv) {
 	/* Version and exit info */
@@ -14,14 +38,16 @@ int main(int argc, char ** argv) {
 
 	/* continuous loop */
 	while (1) {
-		/* Show prompt */
-		fputs(PROMPT, stdout);
+		/* Output prompt & get input */
+		char* line = readline(PROMPT);
 
-		/* Read a line of user input */
-		fgets(input, MAX, stdin);
+		/* Add to history */
+		add_history(line);
 
 		/* Echo it back - actual processing will be later added*/
-		printf("%s kay mhantos, Kapuskondyachi goshta sangu?\n", input);
+		printf("%s kay mhantos, Kapuskondyachi goshta sangu?\n", line);
+
+		free(line);
 	}
 
 	return 0;
